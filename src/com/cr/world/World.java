ae2f8a3c2cb84b01853437827bab7c4ba623b0e4
DigohD <digohd1337@gmail.com>
2014-05-22 23:47:49 +0200

    Renderable World

--- src/com/cr/world/World.java
+++ src/com/cr/world/World.java
@@ -1,9 +1,13 @@
 package com.cr.world;
 
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import com.cr.object.Renderable;
 import com.cr.world.tile.GrassTile;
 import com.cr.world.tile.Tile;
 
-public class World{
+public class World implements Renderable{
 	
 	private final int WORLD_SIZE = 100;
 	private Tile[][] terrain = new Tile[WORLD_SIZE][WORLD_SIZE];
@@ -12,6 +16,18 @@ public class World{
 		for(int i = 0; i < WORLD_SIZE; i++)
 			for(int j = 0; j < WORLD_SIZE; j++)
 				terrain[i][j] = new GrassTile();
+	}
+
+	@Override
+	public void render(Graphics2D g) {
+		for(int i = 0; i < WORLD_SIZE; i++)
+			for(int j = 0; j < WORLD_SIZE; j++)
+				terrain[i][j].draw(g, i, j);
+	}
+
+	@Override
+	public BufferedImage getImage() {
+		return null;
 	}
 	
 }
