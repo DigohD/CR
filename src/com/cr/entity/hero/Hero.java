d6653de0bd66a9930272d049a7c2ce771166f96c
DigohD <digohd1337@gmail.com>
2014-05-23 12:34:18 +0200

    Body Added

--- src/com/cr/entity/hero/Hero.java
+++ src/com/cr/entity/hero/Hero.java
@@ -4,6 +4,8 @@ import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
 import com.cr.entity.Mob;
+import com.cr.entity.hero.body.Body;
+import com.cr.entity.hero.body.Head;
 import com.cr.gameEngine.EntityManager;
 import com.cr.gameEngine.Game;
 import com.cr.resource.ImageLoader;
@@ -14,13 +16,17 @@ public class Hero extends Mob{
 	private BufferedImage image;
 	private Vector2f targetVel;
 	
-	static Vector2f position = new Vector2f(100, 100);
+	public static Vector2f position = new Vector2f(100, 100);
 	
 	private Head head;
+	private Body body;
 	
 	public Hero() {
 		super(position);
+		
 		head = new Head();
+		body = new Body();
+		
 		velocity = new Vector2f(0f, 0f);
 		targetVel = new Vector2f(25f, 0);
 		image = ImageLoader.getImage("hero");
@@ -32,6 +38,7 @@ public class Hero extends Mob{
 		velocity.x = approach(targetVel.x, velocity.x, dt);
 		velocity.y = approach(targetVel.y, velocity.y, dt);
 		move(dt);
+		
 		if(position.x >= Game.WIDTH/2){
 			targetVel.y = 25f;
 			targetVel.x = 0;
@@ -40,17 +47,17 @@ public class Hero extends Mob{
 		if(position.y >= Game.HEIGHT/2){
 			targetVel.y = 0;
 			targetVel.x = 0;
-			
 		}
 		
-		
 		head.tick(dt);
+		body.tick(dt);
 	}
 
 	@Override
 	public void render(Graphics2D g) {
 //		g.drawImage(image, (int)position.x, (int)position.y, null);
 		
+		body.render(g);
 		head.render(g);
 	}
 	
