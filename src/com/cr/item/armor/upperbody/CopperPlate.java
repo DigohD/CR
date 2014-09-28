17870291004edc7ab1a6460e2640f7f20f137cf4
DigohD <digohd1337@gmail.com>
2014-09-28 23:40:49 +0200

    Upper Body Items & Copper Breastplate

---
+++ src/com/cr/item/armor/upperbody/CopperPlate.java
@@ -0,0 +1,27 @@
+package com.cr.item.armor.upperbody;
+
+import com.cr.engine.graphics.Screen;
+import com.cr.item.armor.UpperBody;
+
+public class CopperPlate extends UpperBody{
+
+	public CopperPlate(){
+		super("copperplate", 0, 0, 0, 0, "Copper Breastplate");
+	}
+
+	@Override
+	public void activate() {
+		
+	}
+
+	@Override
+	public void render(Screen screen) {
+		
+	}
+
+	@Override
+	public void tick(float dt) {
+		
+	}
+
+}
