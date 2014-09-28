3b5f76b36c9bb614b17496a023c7ded87c0ba6b7
Anders Eriksson <anders__e92@hotmail.com>
2014-09-28 20:45:18 +0200

    networking started

--- src/com/cr/net/server/Server.java
+++ src/com/cr/net/server/Server.java
@@ -1,5 +1,28 @@
 package com.cr.net.server;
 
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.SocketException;
+
 public class Server {
+	
+	private Thread receiveThread, sendThread;
+	private InetAddress ip;
+	private DatagramSocket socket;
+	private int port;
+	
+	public Server(InetAddress ip, int port){
+		this.ip = ip;
+		this.port = port;
+		
+		try {
+			socket = new DatagramSocket(port);
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+		
+	}
+	
 
 }
---
+++ src/com/cr/net/server/ServerMain.java
@@ -0,0 +1,9 @@
+package com.cr.net.server;
+
+public class ServerMain {
+	
+	public static void main(String[] args){
+		
+	}
+
+}
