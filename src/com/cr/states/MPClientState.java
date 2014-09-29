898bbf904a8bf6d17163c5733ccc4a65f5c9bfb8
Anders Eriksson <anders__e92@hotmail.com>
2014-09-29 19:35:31 +0200

    mp progress

--- src/com/cr/states/MPClientState.java
+++ src/com/cr/states/MPClientState.java
@@ -1,12 +1,15 @@
 package com.cr.states;
 
 import com.cr.engine.graphics.Screen;
+import com.cr.game.EntityManager;
 import com.cr.game.GameStateManager;
 import com.cr.net.client.Client;
+import com.cr.world.World;
 
 public class MPClientState extends GameState{
 	
 	private static Client client;
+	World w;
 
 	public MPClientState(GameStateManager gsm) {
 		super(gsm);
@@ -16,22 +19,24 @@ public class MPClientState extends GameState{
 	
 	@Override
 	public void init() {
+		w = new World();
 		client = new Client("192.168.0.2");
 		client.start();
 		String userName = "anders";
+		EntityManager.getHero().setUserName(userName);
 		String message = "00" + userName;
 		client.sendData(message.getBytes());
 	}
 
 	@Override
 	public void tick(float dt) {
-		
+		w.tick(dt);
 		
 	}
 
 	@Override
 	public void render(Screen screen) {
-		
+		w.render(screen);
 		
 	}
 	
