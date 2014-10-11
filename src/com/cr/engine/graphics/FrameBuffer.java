9d8131e8ec24090c079e196c3d8f37c3d5900b42
Anders Eriksson <anders__e92@hotmail.com>
2014-10-11 14:45:45 +0200

    fb continued

---
+++ src/com/cr/engine/graphics/FrameBuffer.java
@@ -0,0 +1,30 @@
+package com.cr.engine.graphics;
+
+import static org.lwjgl.opengl.GL11.GL_NEAREST;
+import static org.lwjgl.opengl.GL11.GL_RGBA;
+import static org.lwjgl.opengl.GL11.GL_RGBA8;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MAG_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_MIN_FILTER;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_S;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_WRAP_T;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11.glBindTexture;
+import static org.lwjgl.opengl.GL11.glGenTextures;
+import static org.lwjgl.opengl.GL11.glTexImage2D;
+import static org.lwjgl.opengl.GL11.glTexParameterf;
+import static org.lwjgl.opengl.GL11.glTexParameteri;
+import static org.lwjgl.opengl.GL12.GL_CLAMP_TO_EDGE;
+
+import java.nio.ByteBuffer;
+
+public class FrameBuffer {
+	
+	private int fboID;
+	
+	public FrameBuffer(int width, int height){
+		
+	}
+
+	
+}
